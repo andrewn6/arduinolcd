b4432adad635c3ff0041358205685032b7a97e74
Andrew <andrewnijmeh1@gmail.com>
2020-09-17 20:52:26 -0400

    remove crap

--- app.cpp
+++ app.cpp
@@ -7,9 +7,6 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 
 void setup(){
   lcd.begin(16,2) // telling it the amount of spacce on the lcd (16 columns/2 rows)
-  lcd.print("Hello World");
-  lcd.setCursor(0,1);
-  lcd.print("Wow look at this soo cool haha")
 }
 
 // main function to loop over progam
